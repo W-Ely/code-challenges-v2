10ee0c8af6f1d8281d0aa2351fa7b5f818a32001
W Ely Paysinger <paysinger@gmail.com>
2022-12-28 15:44:28 -0800

    leet code: min window

--- src/leet_code/minimum_window_substring.py
+++ src/leet_code/minimum_window_substring.py
@@ -5,102 +5,55 @@ included in the window. If there is no such substring, return the empty string
 "".
 """
 import math
-from queue import PriorityQueue
+from collections import Counter
 
 
-class Graph:
-    def __init__(self, string1, string2):
-        char_counts = {}
-        for char in string2:
-            char_counts[char] = char_counts.get(char, 0) + 1
+def find_min_slice(string1, string2):
+    shortest_length = math.inf
+    shortest_slice = ""
+    max_start = len(string1) - len(string2)
+    if max_start < 0:
+        return shortest_slice
 
-        neighbors = set()
-        slice_graph = {}
-        last_possible_start = math.inf
-        for i in range(len(string1) - 1, -1, -1):
-            if string1[i] not in char_counts:
+    char_counts = Counter(string2)
+    start = end = 0
+    start_char = string1[start]
+    if start_char in char_counts:
+        char_counts[start_char] -= 1
+
+    while end < len(string1) and start <= max_start:
+        end_incremented = start_incremented = False
+        if all(count <= 0 for count in char_counts.values()):
+            length = end - start
+            if shortest_length > length:
+                shortest_length = length
+                shortest_slice = string1[start : end + 1]
+        while end < len(string1) - 1:
+            end_incremented = True
+            end += 1
+            end_char = string1[end]
+            if end_char not in char_counts:
                 continue
-            if len(neighbors) == len(string2):
-                last_possible_start = i
-            g[i] = neighbors.copy()
-            neighbors.add(i)
-
-        self.last_possible_start = last_possible_start
-        self.char_counts = char_counts
-        self.slice_graph = slice_graph
-
-    def get_min_slice(self):
-        queue = PriorityQueue()
-        for i in self.slice_graph:
-            counts = self.char_counts.copy()
-            counts[string1[i]] -= 1
-            queue.put((0, i, counts))
-            if i == self.last_possible_start:
+            char_counts[end_char] -= 1
+            if char_counts[end_char] <= 0:
                 break
+        while start <= max_start and char_counts.get(string1[start], -1) < 0:
+            start_incremented = True
+            start_char = string1[start]
+            if start_char not in char_counts:
+                start += 1
+                continue
+            start += 1
+            char_counts[start_char] += 1
+        if not start_incremented and not end_incremented:
+            break
 
-        shortest_length = math.inf
-        shortest_slice = ""
-
-        while queue.qsize() > 0:
-            previous_length, current, counts = queue.get()
-            for index in current.neighbors:
-                counts = self.char_counts.copy()
-                distance = previous_distance + math.dist(
-                    gate.location, current.location
-                )
-                if gate == self.goal:
-                    if distance < shortest_distance:
-                        shortest_route = route
-                        shortest_distance = distance
-                else:
-                    visited_distance = visited.get(gate, math.inf)
-                    if visited_distance > distance:
-                        queue.put((distance, gate, route + [gate]))
-                        visited[gate] = distance
-        if shortest_distance < math.inf:
-            return self._route(shortest_route)
-        return "Oh for crying out loud..."
-
+    return shortest_slice
 
 
 class Solution:
     """LeetCode entry class"""
 
-    # def minWindow(self, string1, string2):
-    #     """LeetCode entry method"""
-    #     char_counts = {}
-    #     for char in string2:
-    #         char_counts[char] = char_counts.get(char, 0) + 1
-    #
-    #     shortest_length = math.inf
-    #     shortest_slice = ""
-    #
-    #     slice_tracker = {}
-    #
-    #     for i, char in enumerate(string1):
-    #         if char not in char_counts:
-    #             continue
-    #
-    #         slice_tracker[i] = char_counts.copy()
-    #
-    #         start_indexes_to_remove = []
-    #         for start_index, start_index_char_counts in slice_tracker.items():
-    #             if char in start_index_char_counts:
-    #                 start_index_char_counts[char] -= 1
-    #                 if start_index_char_counts[char] == 0:
-    #                     del start_index_char_counts[char]
-    #             if not start_index_char_counts:
-    #                 start_indexes_to_remove.append(start_index)
-    #                 if i - start_index < shortest_length:
-    #                     shortest_length = i - start_index
-    #                     shortest_slice = string1[start_index:i + 1]
-    #
-    #         for start_index in start_indexes_to_remove:
-    #             del slice_tracker[start_index]
-    #
-    #     return shortest_slice
-
-    def minWindow(self, string1, string2):
+    def minWindow(self, string1, string2):  # pylint: disable=invalid-name
         """LeetCode entry method"""
-        graph = Graph(string1, string2)
-        return graph.get_min_slice()
+        return find_min_slice(string1, string2)
